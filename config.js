8ce370fafd460240e223f74e3b99c1771373bd3c
anthonyschurz <anthony.schurz@gmail.com>
2016-05-30 14:13:12 -0700

    trumpifying

---
+++ config.js
@@ -0,0 +1,12 @@
+var config = {};
+
+// These credentials were obtained from creating a new
+// developer project on http://apps.twitter.com.
+config.twitter = {
+  consumer_key: '8yDPIwfnf8dt4HXKVFFD6NxjM',
+  consumer_secret: 'lFOq32QSYoOI7wAmrOTCZtNZBVQfsH7DuJoiEzwxDcR5faszRq',
+  access_token_key: '309081412-kWQwXdgopm4jM5kqKTUItr4mEdYbKN4PE9MLdY9g',
+  access_token_secret: '6FqRoKNGoRXFkoJH718yWsmTrfO7H8uzznwLDhGb63jrz'
+};
+
+module.exports = config;
