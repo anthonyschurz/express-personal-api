dffb219ce61661f663d90cddafc89f068b96f783
anthonyschurz <anthony.schurz@gmail.com>
2016-06-02 08:00:19 -0700

    Adds Frontend

--- models/index.js
+++ models/index.js
@@ -3,4 +3,5 @@ mongoose.connect( process.env.MONGOLAB_URI ||
                   process.env.MONGOHQ_URL ||
                   "mongodb://localhost/personal-api");
 
-// module.exports.Campsite = require("./campsite.js.example");
+module.exports.Visits = require("./places_visited");
+module.exports.Volunteering = require("./volunteering");
